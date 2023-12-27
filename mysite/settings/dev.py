9adfb24a0918ab5365aafe9976e5bd844293d56b
Jędrzej Grabala <behemotpl@gmail.com>
2023-12-27 11:50:38 +0000

    initrepo

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-cbs1xd3b+f5e*s$g32xozp*^1h%u$5a$x1o_@j=mg4!bd6oxqc"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
